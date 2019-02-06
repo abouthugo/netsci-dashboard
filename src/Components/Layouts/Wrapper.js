248d843c4b8d9ca4e6a3427fef537d5746733121
Hugo Perdomo <hugo@Hugos-Mac18.local>
2019-02-06 17:05:24 -0500

    Changed grid system to be 5x5 grid instead of previous are template

--- src/Components/Layouts/Wrapper.js
+++ src/Components/Layouts/Wrapper.js
@@ -3,12 +3,12 @@ import styled from 'styled-components';
 const Wrapper = styled.div`
   display: grid;
   grid-template-areas: 
-  "side dash dash dash dash"
-  "side dash dash dash dash"
-  "side dash dash dash dash"
-  "side dash dash dash dash";
-  grid-column-gap: 10px;
-  grid-row-gap: 10px;
-  background: rgba(40,100,200,0.2);
+  "side dash dash dash dash dash dash dash dash dash dash dash" ;
+  grid-gap: 10px 10px;
+  background: #F5F6FA;
+  min-height: 99.9vh;
+  max-height: 100vh;
+  min-width: 99.9vw;
+  max-width: 100vw;
 `;
 export default Wrapper;
