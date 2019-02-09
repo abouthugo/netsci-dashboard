5692ce37e18d417018989435ba350d97b8c82333
Hugo Perdomo <hugo@Hugos-Mac18.home>
2019-02-09 14:40:08 -0500

    Bundled button on left panel container to randomize data on chart components

---
+++ src/Components/LeftPanel/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import NavButton from '../NavButton';
+import SidePanel from '../Layouts/SidePanel';
+import { DataContext } from '../../Context';
+
+const LeftPanel = () => (
+    <SidePanel>
+        <DataContext.Consumer>
+            { context =>
+                <>
+                    <NavButton onClick={ context.randomPie }>🎲Pie</NavButton>
+                    <NavButton onClick={ context.randomLine }>🎲Line</NavButton>
+                    <NavButton onClick={ context.randomBar }>🎲Bar</NavButton>
+                    <NavButton onClick={ context.randomRadar }>🎲Radar</NavButton>
+                </>
+            }
+        </DataContext.Consumer>
+    </SidePanel>
+);
+
+export default LeftPanel;
---
+++ src/Components/NavButton/index.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const NavButton = styled.button`
+  border: 1px solid orangered;
+  border-radius: 5px;
+  //justify-self: center;
+  //align-self: center;
+  padding: 5px 20px;
+  margin: 10px 5px;
+`;
+
+export default NavButton;
