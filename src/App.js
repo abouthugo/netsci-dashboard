c6b63df22bf6bd6d5c9fdaef4bdc0f1c9b3c3dab
Hugo Perdomo <hugo.j.dev@gmail.com>
2019-04-04 11:20:47 -0400

    Refactoring some layouts, deleted DashboardCardsGrid and introduced Grid component in DashboardPanel

--- src/App.js
+++ src/App.js
@@ -1,44 +1,38 @@
-import React from 'react';
-import './App.scss'; // Google Font
-import Wrapper from './Components/Layouts/Wrapper';
-import DashboardPanel from './Components/Layouts/DashboardPanel';
-import DashboardCardsGrid from './Components/Layouts/DashboardCardsGrid';
-import DashboardCard from './Components/Layouts/DashboardCard';
-import BarChart from "./Components/BarChart";
-import LineChart from "./Components/LineChart";
-import PieChart from "./Components/PieChart";
-import RadarChart from "./Components/RadarChart";
-import { DataProvider } from "./Context";
-import LeftPanel from './Components/LeftPanel';
+import React from 'react'
+import './App.scss' // Google Font
+import Wrapper from './Components/Layouts/Wrapper'
+import DashboardPanel from './Components/Layouts/DashboardPanel'
+import DashboardCard from './Components/Layouts/DashboardCard'
+import BarChart from './Components/BarChart'
+import LineChart from './Components/LineChart'
+import PieChart from './Components/PieChart'
+import RadarChart from './Components/RadarChart'
+import { DataProvider } from './Context'
+import LeftPanel from './Components/LeftPanel'
 
 const App = () => (
-    <DataProvider>
-        <Wrapper>
-            <LeftPanel/>
-            <DashboardPanel>
-                <DashboardCardsGrid>
+  <DataProvider>
+    <Wrapper>
+      <LeftPanel />
+      <DashboardPanel>
+        <DashboardCard column='1/3' row='1/7'>
+          <PieChart />
+        </DashboardCard>
 
-                    <DashboardCard column="1/3" row="1/7">
-                        <PieChart/>
-                    </DashboardCard>
+        <DashboardCard column='3/11' row='1/7'>
+          <BarChart />
+        </DashboardCard>
 
-                    <DashboardCard column="3/11" row="1/7">
-                        <BarChart />
-                    </DashboardCard>
+        <DashboardCard column='1/4' row='7/11'>
+          <RadarChart />
+        </DashboardCard>
 
-                    <DashboardCard column="1/4" row="7/11">
-                        <RadarChart/>
-                    </DashboardCard>
+        <DashboardCard column='4/11' row='7/11'>
+          <LineChart />
+        </DashboardCard>
+      </DashboardPanel>
+    </Wrapper>
+  </DataProvider>
+)
 
-                    <DashboardCard column="4/11" row="7/11">
-                        <LineChart/>
-                    </DashboardCard>
-
-                </DashboardCardsGrid>
-            </DashboardPanel>
-        </Wrapper>
-    </DataProvider>
-);
-
-
-export default App;
+export default App
--- src/Components/Layouts/DashboardPanel.js
+++ src/Components/Layouts/DashboardPanel.js
@@ -1,6 +1,27 @@
+import React from 'react';
 import styled from 'styled-components';
 
+/**
+ * Division wrapper for panel section
+ */
 const DashboardPanel = styled.section`
   grid-area: dash;
 `;
-export default DashboardPanel;
+
+/**
+ * 10x10 grid
+ */
+const Grid = styled.div`
+margin: 10px 20px 0 20px;
+display: grid;
+grid-template-columns: repeat(10, 1fr);
+grid-template-rows: repeat(10, 1fr);
+min-height: 97%;
+grid-gap: 20px;
+`;
+
+export default ({children}) => (
+  <DashboardPanel>
+    <Grid>{children}</Grid>
+  </DashboardPanel>
+);
